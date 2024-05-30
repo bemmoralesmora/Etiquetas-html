07753b3dfebb631f173f5f107df2b404640bdc53
jerónimo <sajeronimo@scl.edu.gt>
2024-05-30 13:33:54 -0600

    Etiquetas de texto de jeronimo

--- section2/section2.js
+++ section2/section2.js
@@ -1,5 +1,17 @@
 let section2_dom = document.querySelector(".section2");
 
 section2_dom.innerHTML= `
-hola s2
-`;
+<h1>Etiquetas de Texto</h1>
+<table>
+<tr><th>Etiqueta</th><th>Descripción</th></tr>
+<tr><td>&lt;p&gt;</td><td>Define un párrafo.</td></tr>
+<tr><td>&lt;h1&gt; a &lt;h6&gt;</td><td>Define encabezados, de &lt;h1&gt; (más importante) a &lt;h6&gt; (menos importante).</td></tr>
+<tr><td>&lt;a&gt;</td><td>Define un hipervínculo.</td></tr>
+<tr><td>&lt;b&gt;</td><td>Define texto en negrita.</td></tr>
+<tr><td>&lt;i&gt;</td><td>Define texto en cursiva.</td></tr>
+<tr><td>&lt;u&gt;</td><td>Define texto subrayado.</td></tr>
+<tr><td>&lt;strong&gt;</td><td>Define texto importante (generalmente se muestra en negrita).</td></tr>
+<tr><td>&lt;em&gt;</td><td>Define texto enfatizado (generalmente se muestra en cursiva).</td></tr>
+<tr><td>&lt;small&gt;</td><td>Define texto pequeño.</td></tr>
+</table>
+   `;
